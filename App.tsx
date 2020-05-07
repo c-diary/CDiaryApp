8a93100c9cf56f6dd843fa4435e38f2ea05d2e10
Christian M <chriamue@gmail.com>
2020-05-07 08:14:17 +0200

    :lipstick: :sparkles: added tab navigator

--- App.tsx
+++ App.tsx
@@ -9,115 +9,38 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import DP3TScreen from './src/DP3TScreen';
 
-import { DP3T } from 'c-diary-rn';
+const Tab = createBottomTabNavigator();
 
-declare const global: { HermesInternal: null | {} };
+interface Props { }
+interface State {
+}
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <DP3T
-                backendAppId={'org.dpppt.demo'}
-                publicKeyBase64={
-                  'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0R' +
-                  'RZ0FFSndKMkErS2taR0p6QlMzM3dEOUUyaEI1K3VNYgpZcitNU2pOUGhmYzR6Q2w2amdSWkFWVHBKbE' +
-                  '0wSmI4RERqcDNRUDZhK2VEK1I1SFYyNzhROVN0SUhnPT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0t'
-                }
-              />
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+class App extends React.Component<Props, State>{
+  constructor(props: any) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="DP3T" component={DP3TScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </>
+    );
+  }
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
---
+++ src/DP3TScreen.tsx
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import {
+    View,
+} from 'react-native';
+
+import { DP3T } from 'c-diary-rn';
+
+interface Props { }
+interface State {
+    backendAppId: string,
+    publicKeyBase64: string
+}
+
+class DP3TScreen extends React.Component<Props, State>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            backendAppId: 'org.dpppt.demo',
+            publicKeyBase64:
+                'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0R' +
+                'RZ0FFSndKMkErS2taR0p6QlMzM3dEOUUyaEI1K3VNYgpZcitNU2pOUGhmYzR6Q2w2amdSWkFWVHBKbE' +
+                '0wSmI4RERqcDNRUDZhK2VEK1I1SFYyNzhROVN0SUhnPT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0t'
+        };
+    }
+
+    render() {
+        const { backendAppId, publicKeyBase64 } = this.state;
+        return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <DP3T
+                backendAppId={backendAppId}
+                publicKeyBase64={publicKeyBase64}
+            />
+        </View>
+        );
+    }
+};
+
+export default DP3TScreen;
